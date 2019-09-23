5606283a2c516626e3fefd0512009f9d4f850d30
Cloe Kouadjo <armelfli50@gmail.com>
2019-09-23 21:46:01 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -5,7 +5,7 @@ class CreateHauntedHouses < ActiveRecord[5.1]
       t.string :name 
       t.string :loction
       t.string :theme 
-      t.integer :price 
+      t.float :price 
       t.boolean :family_friendly 
       t.string :opening_date 
       t.string :closing_date 
