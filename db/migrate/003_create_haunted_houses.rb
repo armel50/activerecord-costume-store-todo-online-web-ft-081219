503e5f14e69aec0c7f9b31e8a5fd355cc1040617
Cloe Kouadjo <armelfli50@gmail.com>
2019-09-23 21:52:08 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,7 +3,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   def change 
     create_table :haunted_houses do |t| 
       t.string :name 
-      t.address :loction
+      t.text :loction
       t.string :theme 
       t.float :price 
       t.boolean :family_friendly 
